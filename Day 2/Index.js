4b3a9b6caa2f9a4bfa75c49b569e62eb93260beb
Leon Hayden <leon.hayden@gmx.com>
2018-06-05 15:30:28 -0500

    completed assignment

--- Day 2/Index.js
+++ Day 2/Index.js
@@ -4,6 +4,7 @@ const form = document.querySelector('form')
 
 const changeHeading = function(ev) {
 
+console.log("run the function")
   ev.preventDefault()
 
 
@@ -12,12 +13,13 @@ const changeHeading = function(ev) {
 
   const spellName = f.spellName.value 
 
-  const spellNameA = f.spellNameA.value  
-
-
+  const spellNameA = f.spellNameA.value
   const spellsDiv = document.querySelector('#spells')
-
+    
+  const spellsDivA = document.querySelector('#spells')
+  
   spellsDiv.innerHTML += `<li>${spellName}</li>`
+  spellsDivA.innerHTML += `<li>${spellNameA}</li>`
 
 
 
