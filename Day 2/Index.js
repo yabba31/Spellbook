eaa0b4ddcc3f2f095fd88759952bb8f0ca9ab2d3
Leon Hayden <leon.hayden@gmx.com>
2018-06-05 14:15:04 -0500

    intial start

---
+++ Day 2/Index.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('form')
+
+
+
+const changeHeading = function(ev) {
+
+  ev.preventDefault()
+
+
+
+  const f = ev.target
+
+  const spellName = f.spellName.value 
+
+  const spellNameA = f.spellNameA.value  
+
+
+  const spellsDiv = document.querySelector('#spells')
+
+  spellsDiv.innerHTML += `<li>${spellName}</li>`
+
+
+
+  f.reset()
+
+}
+
+
+
+form.addEventListener('submit', changeHeading)
